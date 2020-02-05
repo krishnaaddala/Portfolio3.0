45e9fb554ad6f0290597ca7ae57002d40b3e94a9
Krishna <kaddala@salesforce.com>
2020-02-04 23:13:32 -0800

    placing in some pseudo code and uncommenting chunks on PORTFOLIO page

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,6 @@ INSERT INTO Projects (title, image, gitrepo, deployedapp)
 VALUES ("Word Game Guess", "assets/images/WordGameGuess.png", "https://krishnaaddala.github.io/Word-Guess-Game/", "https://krishnaaddala.github.io/Word-Guess-Game/"),
 ("Word Game Guess", "assets/images/WordGameGuess.png", "https://krishnaaddala.github.io/Word-Guess-Game/", "https://krishnaaddala.github.io/Word-Guess-Game/")
 
+-- unfortunately this SQL saving did not work as it kept throwing an error to include timestamp related info while trying the inserts. 
 
 
